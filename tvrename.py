e7fe0f6d0a6aef954561e36f6be5c5a13c69c952
Noah Cou <35743816+noahcou@users.noreply.github.com>
2021-12-15 18:41:55 -0500

    Update tvrename.py
    updated it with minor changes I made overtime

--- tvrename.py
+++ tvrename.py
@@ -1,15 +1,15 @@
 # import stuff
-from os import listdir, rename, remove
+from os import listdir, rename
 
 # get show name and season
-show = input("Input name of the show\n")
-sea = input("\nInput the season number in double digits (01, 02, etc)\n")
-ext = input("\nWhat is the file extension? (mp4, mkv, etc)\n")
+show = input("Input name of the show\n").strip()
+sea = input("\nInput the season number in double digits (01, 02, etc)\n").strip()
+# ext = input("\nWhat is the file extension? (mp4, mkv, etc)\n")
 info = input("\nWant extra info? (Y/N): ")[0].lower()
 
 # get extra info if necessary
 if info == "y":
-    info = input("What info would you like every episode to have displayed at the end?\n")
+    info = input("What info would you like every episode to have displayed at the end?\n").strip()
     ex = True
 else:
     ex = False
@@ -21,7 +21,7 @@ ep = 1
 for x in listdir():
 
     # find the files that end with the desired extension
-    if x.endswith(ext):
+    if x.endswith("mkv" or "mp4"):
 
         # format episode number
         if ep < 10:
@@ -31,9 +31,9 @@ for x in listdir():
         
         # rename based on extra info
         if ex:
-            rename(x, f"{show} - s{sea}e{eps} - {info}.{ext}")
+            rename(x, f"{show} - s{sea}e{eps} - {info}.{x.split('.')[-1]}")
         else:
-            rename(x, f"{show} - s{sea}e{eps}.{ext}")
+            rename(x, f"{show} - s{sea}e{eps}.{x.split('.')[-1]}")
         
         #increment episode number
         ep += 1
