560b97145307c77adc5b4c0ff2ca6fc992459aa4
bunnycou <35743816+bunnycou@users.noreply.github.com>
2023-08-26 09:40:29 -0400

    Updated the script to automatically do an entire directory of shows instead of having to run it for each different season

--- tvrename.py
+++ tvrename.py
@@ -1,39 +1,21 @@
 # import stuff
-from os import listdir, rename
+from os import listdir, rename, scandir
 
-# get show name and season
-show = input("Input name of the show\n").strip()
-sea = input("\nInput the season number in double digits (01, 02, etc)\n").strip()
-# ext = input("\nWhat is the file extension? (mp4, mkv, etc)\n")
-info = input("\nWant extra info? (Y/N): ")[0].lower()
+# automatically rename entire shows
+for folder in scandir("."): 
+    if folder.is_dir(): #make sure random files aren't inlcuded
+        show = folder.name
+        for season in scandir(folder):
+            if season.name.lower().startswith("season"): # make sure we only get season folders
+                sea = season.name.lower()[7:]
+                ep = 1
+                for episode in scandir(season):
+                    if episode.endswith("mkv") or episode.endswith("mp4") or episode.endswith("webm"): # make sure we target media files
+                        # format episode number
+                        if ep < 10:
+                            eps = f"0{ep}"
+                        else:
+                            eps = f"{ep}"
 
-# get extra info if necessary
-if info == "y":
-    info = input("What info would you like every episode to have displayed at the end?\n").strip()
-    ex = True
-else:
-    ex = False
-
-#episode counter
-ep = 1
-
-# iterate through all files in the current directory
-for x in listdir():
-
-    # find the files that end with the desired extension
-    if x.endswith("mkv") or x.endswith("mp4"):
-
-        # format episode number
-        if ep < 10:
-            eps = f"0{ep}"
-        else:
-            eps = f"{ep}"
-        
-        # rename based on extra info
-        if ex:
-            rename(x, f"{show} - s{sea}e{eps} - {info}.{x.split('.')[-1]}")
-        else:
-            rename(x, f"{show} - s{sea}e{eps}.{x.split('.')[-1]}")
-        
-        #increment episode number
-        ep += 1
+                        rename(episode, f"{show} - s{sea}e{eps}.{episode.name.split('.')[-1]}")
+                        ep += 1
