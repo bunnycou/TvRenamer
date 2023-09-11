2f7ee969f634e4a779adb3ebeffd01882ef311f8
bunnycou <35743816+bunnycou@users.noreply.github.com>
2023-09-10 20:55:57 -0400

    Update tvrename.py
    fixed error with renaming

--- tvrename.py
+++ tvrename.py
@@ -1,5 +1,5 @@
 # import stuff
-from os import listdir, rename, scandir
+from os import rename, scandir
 
 # automatically rename entire shows
 for folder in scandir("."): 
@@ -10,12 +10,13 @@ for folder in scandir("."):
                 sea = season.name.lower()[7:]
                 ep = 1
                 for episode in scandir(season):
-                    if episode.endswith("mkv") or episode.endswith("mp4") or episode.endswith("webm"): # make sure we target media files
+                    if episode.name.endswith("mkv") or episode.name.endswith("mp4") or episode.name.endswith("webm"): # make sure we target media files
                         # format episode number
                         if ep < 10:
                             eps = f"0{ep}"
                         else:
                             eps = f"{ep}"
-
-                        rename(episode, f"{show} - s{sea}e{eps}.{episode.name.split('.')[-1]}")
+                            
+                        path = "\\".join(episode.path.split("\\")[:3])
+                        rename(episode, f"{path}/{show} - s{sea}e{eps}.{episode.name.split('.')[-1]}")
                         ep += 1
